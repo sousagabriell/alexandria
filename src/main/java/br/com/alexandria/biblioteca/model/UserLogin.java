c1b355a3ba449fffb783398adf2f6ea714f6077b
Gustavo <gustavocoliver@gmail.com>
2022-10-14 03:14:30 -0300

    Security

---
+++ src/main/java/br/com/alexandria/biblioteca/model/UserLogin.java
@@ -0,0 +1,23 @@
+package br.com.alexandria.biblioteca.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLogin {
+
+	private String nome;
+	
+	
+	private String usuario;
+	
+	
+	private String senha;
+	
+	
+	private String token;
+
+
+}
---
+++ src/main/java/br/com/alexandria/biblioteca/model/Usuario.java
@@ -0,0 +1,46 @@
+package br.com.alexandria.biblioteca.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_usuarios")
+@Getter
+@Setter
+public class Usuario {
+
+	@Id
+	@Column(name = "id_usuario")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "nome_usuario")
+	@NotNull
+	private String nome;
+
+	@Column(name = "email_usuario")
+	@NotNull
+	private String email;
+
+	@Column(name = "usuario")
+	@NotNull
+	private String usuario;
+
+	@Column(name = "foto_usuario")
+	@NotNull
+	private String foto;
+
+	@Column(name = "senha_usuario")
+	@NotNull
+	@Size(min = 8)
+	private String senha;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data_usuario = new java.sql.Date(System.currentTimeMillis());
+
+}
